fba19098d0dd98fad3d6580c62e3d0eee4e7e555
Chanung Yun <jjanung.yun@gmail.com>
2015-12-09 23:30:52 +0900

    chap1-5

--- demo/src/test/java/com/example/user/dao/UserDaoTest.java
+++ demo/src/test/java/com/example/user/dao/UserDaoTest.java
@@ -2,11 +2,15 @@ package com.example.user.dao;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.example.user.User;
 
 public class UserDaoTest {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new DaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("whiteship");
