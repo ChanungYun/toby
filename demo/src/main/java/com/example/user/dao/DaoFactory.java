028d0a406999b812f674b120ab8faa5acb62979c
Chanung Yun <jjanung.yun@gmail.com>
2015-12-14 20:41:15 +0900

    chap1-8

--- demo/src/main/java/com/example/user/dao/DaoFactory.java
+++ demo/src/main/java/com/example/user/dao/DaoFactory.java
@@ -1,17 +1,28 @@
 package com.example.user.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
 	@Bean
 	public UserDao userDao() {
-		return new UserDao(connectionMaker());
+		UserDao userDao = new UserDao();
+		userDao.setDataSource(dataSource());
+		return userDao;
 	}
 	
 	@Bean
-	public ConnectionMaker connectionMaker() {
-		return new DConnectionMaker();
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://localhost/test");
+		dataSource.setUsername("test");
+		dataSource.setPassword("test");
+		
+		return dataSource;
 	}
 }
--- demo/src/test/java/com/example/user/dao/UserDaoTest.java
+++ demo/src/test/java/com/example/user/dao/UserDaoTest.java
@@ -3,24 +3,18 @@ package com.example.user.dao;
 import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.example.user.User;
 
 public class UserDaoTest {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		DaoFactory factory = new DaoFactory();
-		UserDao dao1 = factory.userDao();
-		UserDao dao2 = factory.userDao();
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {		
+		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
-		System.out.println(dao1);
-		System.out.println(dao2);
+		User user1 = dao.get("whiteship");
+		System.out.println(user1.getName());
+		System.out.println(user1.getPassword());
 		
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-		UserDao dao3 = context.getBean("userDao", UserDao.class);
-		UserDao dao4 = context.getBean("userDao", UserDao.class);
-		
-		System.out.println(dao3);
-		System.out.println(dao4);
 	}
 }
