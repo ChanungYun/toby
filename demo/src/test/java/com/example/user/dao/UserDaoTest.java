c1982da075286962c9b37d8f659bfa6dc69dffe2
Chanung Yun <jjanung.yun@gmail.com>
2015-12-14 21:00:36 +0900

    chap2-2

--- demo/src/test/java/com/example/user/dao/UserDaoTest.java
+++ demo/src/test/java/com/example/user/dao/UserDaoTest.java
@@ -1,20 +1,34 @@
 package com.example.user.dao;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 import java.sql.SQLException;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import com.example.user.User;
 
 public class UserDaoTest {
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {		
+	
+	@Test
+	public void addAndGet() throws SQLException {
 		ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+		
 		UserDao dao = context.getBean("userDao", UserDao.class);
+		User user = new User();
+		user.setId("gyumee");
+		user.setName("¹Ú¼ºÃ¶");
+		user.setPassword("springno1");
 		
-		User user1 = dao.get("whiteship");
-		System.out.println(user1.getName());
-		System.out.println(user1.getPassword());
+		dao.add(user);
 		
+		User user2 = dao.get(user.getId());
+		
+		assertThat(user2.getName(), is(user.getName()));
+		assertThat(user2.getPassword(), is(user.getPassword()));
 	}
+	
 }
