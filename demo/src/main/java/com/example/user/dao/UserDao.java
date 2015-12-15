319a2873640a3a194341cdd7ea00b18d7e44b034
Chanung Yun <jjanung.yun@gmail.com>
2015-12-15 22:32:59 +0900

    chap3-6

--- demo/src/main/java/com/example/user/dao/UserDao.java
+++ demo/src/main/java/com/example/user/dao/UserDao.java
@@ -4,86 +4,68 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import com.example.user.User;
 
 public class UserDao {
-	private DataSource dataSource;
-	private JdbcContext jdbcContext;
-	
-	
-	public void setDataSource(DataSource dataSource) {
-		jdbcContext = new JdbcContext();
-		jdbcContext.setDataSource(dataSource);
-		this.dataSource = dataSource;
-	}
+	private JdbcTemplate jdbcTemplate;
 
-	public void add(User user) throws SQLException {
-		jdbcContext.workWithStatementStrategy(new StatementStrategy() {
-			
-			@Override
-			public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-				PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-				ps.setString(1, user.getId());
-				ps.setString(2, user.getName());
-				ps.setString(3, user.getPassword());
-				return ps;
-			}
-		});
-	}
-	
-	public User get(String id) throws SQLException {
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
-		
-		ResultSet rs = ps.executeQuery();
-		
-		User user = null;
-		if (rs.next()) {
-			user = new User();
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
 			user.setId(rs.getString(1));
 			user.setName(rs.getString(2));
 			user.setPassword(rs.getString(3));
+			return user;
 		}
 		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		if (user == null) throw new EmptyResultDataAccessException(1);
-		
-		return user;	
+	};
+	
+	public void setDataSource(DataSource dataSource) {
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public void add(User user) throws SQLException {
+		jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)", 
+				user.getId(), user.getName(), user.getPassword());
+	}
+	
+	public User get(String id) throws SQLException {
+		return jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] {id}, this.userMapper);
 	}
 	
 	public void deleteAll() throws SQLException {
-		jdbcContext.executeSql("delete from users");
+		jdbcTemplate.update("delete from users");
 	}
 	
 	public int getCount() throws SQLException {
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		
-		try {
-			c = dataSource.getConnection();
+		return jdbcTemplate.query(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				return con.prepareStatement("select count(*) from users");
+			}
+		}, new ResultSetExtractor<Integer>() {
+			@Override
+			public Integer extractData(ResultSet rs) throws SQLException, DataAccessException {
+				rs.next();
+				return rs.getInt(1);
+			}
 			
-			ps = c.prepareStatement("select count(*) from users");
-			
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		} finally {
-			if (rs != null) { try { rs.close(); } catch (SQLException e) {}}
-			if (ps != null) { try { ps.close(); } catch (SQLException e) {}}
-			if (c != null) { try { c.close(); } catch (SQLException e) {}}
-		}	
+		});
 	}
+
+	public List<User> getAll() {
+		return jdbcTemplate.query("select * from users order by id", this.userMapper);
+	}
+	
 }
